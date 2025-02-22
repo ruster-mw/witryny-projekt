ddb1cea67027bf4ce54c3b655df6377b35b63acd
ruster-mw <mateuszwypior2000@gmail.com>
2025-02-22 19:10:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let hamburgerButton = document.querySelector('.hamburger-przycisk');
+let menuMobilne = document.querySelector('.mobilne-menu');
+
+function pokazMenu(){
+    menuMobilne.classList.toggle('menu-pokaz')
+};
+
